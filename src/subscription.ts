7d7415904d9fc165af832f5075ca4081647e93c9
mrlctate <1757089@qq.com>
2024-10-17 21:43:15 +0800

    feat(百度网盘): 开屏广告(规则#002)

--- src/subscription.ts
+++ src/subscription.ts
@@ -5,11 +5,11 @@ import globalGroups from './globalGroups';
 
 export default defineGkdSubscription({
   id: 233,
-  name: 'Subscription',
+  name: '11111111',
   version: 0,
   author: 'author',
   checkUpdateUrl: './gkd.version.json5',
-  supportUri: 'https://github.com/gkd-kit/subscription-template',
+  supportUri: 'https://github.com/mrlctate/subscription-json',
   categories,
   globalGroups,
   apps: await batchImportApps(`${import.meta.dirname}/apps`),
