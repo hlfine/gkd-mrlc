3a9308704e2b20015c6d0d47b80dc66aa0a05117
mrlctate <1757089@qq.com>
2024-10-18 11:53:15 +0800

    feat(百度网盘): 开屏广告(规则#002)

--- src/apps/com.baidu.netdisk.ts
+++ src/apps/com.baidu.netdisk.ts
@@ -5,39 +5,18 @@ export default defineGkdApp({
   name: '百度网盘',
   groups: [
     {
-      name: '全屏广告-弹窗广告',
-      key: 1,
-      desc: '点击关闭',
-      enable: false,
+      name: '开屏广告',
+      key: 0,
+      quickFind: true,
+      actionMaximum: 1,
+      matchTime: 10000,
+      resetMatch: 'app',
       rules: [
         {
-          key: 3,
-          matches: ['[vid="iv_close"]'],
-          snapshotUrls: [
-            'https://i.gkd.li/i/17405893',
-            'https://i.gkd.li/i/12642505',
-            'https://i.gkd.li/i/12783106',
-            'https://i.gkd.li/i/12923937',
-            'https://i.gkd.li/i/13806852',
-            'https://i.gkd.li/i/14730106',
-            'https://i.gkd.li/i/14822799',
-          ],
-          activityIds: [
-            'com.baidu.netdisk.business.guide.dialog.lifeproduct.',
-            'com.baidu.netdisk.ui.MainActivity',
-            'com.baidu.netdisk.ui.Navigate',
-          ],
-        },
-        {
-          name: '相册页面激活无限空间弹窗',
-          key: 4,
-          matches: [
-            '@ImageView[id="com.baidu.netdisk:id/close_btn"] + ImageView[id="com.baidu.netdisk:id/bg_image"]',
-          ],
-          snapshotUrls: ['https://i.gkd.li/i/12648987'],
-          activityIds: [
-            'com.baidu.netdisk.cloudimage.ui.album.AlbumGuideOneImageDialog',
-          ],
+          key: 0,
+          action: 'clickCenter',
+          matches: ['TextView[text*="跳过"][text.length<=10]'],
+          snapshotUrls: ['https://i.gkd.li/i/17405893'],
         },
       ],
     },
