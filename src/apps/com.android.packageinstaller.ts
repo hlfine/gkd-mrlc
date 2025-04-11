e2b30acdd8563f4bbb9876045ea05fdc2f0d421b
mrlctate <1757089@qq.com>
2025-04-11 09:26:14 +0800

    feat: 规则维护更新

--- src/apps/com.android.packageinstaller.ts
+++ src/apps/com.android.packageinstaller.ts
@@ -12,31 +12,35 @@ export default defineGkdApp({
         {
           key: 0,
           name: '点击[继续安装]',
-          activityIds: [
-            'com.android.packageinstaller.PackageInstallerActivity',
-            'com.android.packageinstaller.FlymePackageInstallerActivity',
+          activityIds: ['.InstallStart', '.FlymePackageInstallerActivity'],
+          anyMatches: [
+            '[text="继续安装" && clickable=true]',
+            '@LinearLayout[clickable=true] > [text="继续安装"]',
           ],
-          matches: '[text="继续安装"]',
           snapshotUrls: [
             'https://i.gkd.li/i/13206444',
             'https://i.gkd.li/i/14046749',
             'https://i.gkd.li/i/16550275',
+            'https://i.gkd.li/i/19697192',
+            'https://i.gkd.li/i/19665327',
           ],
         },
         {
           key: 1,
           name: '点击[完成]',
           activityIds: [
-            'com.android.packageinstaller.NewInstallInstalling',
-            'com.android.packageinstaller.InstallSuccess',
-            'com.android.packageinstaller.oplus.InstallAppProgress',
-            'com.android.packageinstaller.FlymePackageInstallerActivity',
+            '.NewInstallInstalling',
+            '.InstallSuccess',
+            '.oplus.InstallAppProgress',
+            '.FlymePackageInstallerActivity',
           ],
-          matches: '[text="完成"]',
+          anyMatches: [
+            '[text="完成" && clickable=true]',
+            '@LinearLayout[clickable=true] > [text="完成"]',
+          ],
           snapshotUrls: [
             'https://i.gkd.li/i/13206476',
             'https://i.gkd.li/i/13766420',
-            'https://i.gkd.li/i/13962438',
             'https://i.gkd.li/i/14138323',
             'https://i.gkd.li/i/14471862',
             'https://i.gkd.li/i/16550273',
@@ -45,22 +49,19 @@ export default defineGkdApp({
         {
           key: 2,
           name: '点击[安装]',
-          fastQuery: true,
-          activityIds: 'com.android.packageinstaller.DeleteStagedFileOnResult',
+          activityIds: '.DeleteStagedFileOnResult',
           matches: '[vid="confirm_bottom_button_layout"]',
           snapshotUrls: 'https://i.gkd.li/i/14228348',
         },
         {
           key: 3,
           name: '勾选[已了解此应用未经检测，可能存在风险]',
-          fastQuery: true,
-          activityIds: 'com.android.packageinstaller.PackageInstallerActivity',
-          matches: '[vid="checkbox"][checked=false]',
+          activityIds: '.PackageInstallerActivity',
+          matches: '[vid="checkbox" && clickable=true]',
           snapshotUrls: 'https://i.gkd.li/i/14595443',
         },
         {
           key: 4,
-          fastQuery: true,
           position: {
             left: 'width * 1.5394',
             top: 'height * 0.5',
--- src/apps/com.oplus.appdetail.ts
+++ src/apps/com.oplus.appdetail.ts
@@ -8,33 +8,28 @@ export default defineGkdApp({
       key: 1,
       name: '功能类-自动安装应用',
       fastQuery: true,
-      activityIds: [
-        '.InstallStart',
-        '.model.guide.ui.InstallGuideActivity',
-        '.model.finish.InstallFinishActivity',
-      ],
       rules: [
         {
           key: 0,
           name: '点击[继续安装]',
+          activityIds: '.model.guide.ui.InstallGuideActivity',
           excludeMatches:
             '[id="com.oplus.appdetail:id/view_scanning_and_tip_view_tv_title"][text^="正在扫描"]',
-          matches: '[text="继续安装" && text.length=4]',
+          matches: '[text="继续安装" && clickable=true]',
           snapshotUrls: [
             'https://i.gkd.li/i/13038560', // 使用 excludeMatches，扫描病毒阶段不进行点击
             'https://i.gkd.li/i/13054204',
             'https://i.gkd.li/i/13038570',
-            'https://i.gkd.li/i/19665327',
           ],
         },
         {
           key: 1,
           name: '点击[完成]',
-          matches: '[text="完成" && text.length=2]',
+          activityIds: '.model.finish.InstallFinishActivity',
+          matches: '[text="完成" && clickable=true]',
           snapshotUrls: [
             'https://i.gkd.li/i/13038664',
             'https://i.gkd.li/i/13054849',
-            'https://i.gkd.li/i/19665319',
           ],
         },
       ],
