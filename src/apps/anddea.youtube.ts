1ab7a51d925b5bf6718e392cf73abe5ac49145e2
mrlctate <1757089@qq.com>
2025-04-13 10:24:16 +0800

    feat: 版本迭代

--- src/apps/anddea.youtube.ts
+++ src/apps/anddea.youtube.ts
@@ -13,7 +13,7 @@ export default defineGkdApp({
           activityIds:
             'com.google.android.apps.youtube.app.watchwhile.MainActivity',
           matches:
-            '@Button[desc^="翻译成中文"][visibleToUser=true] <<n [vid="results"]',
+            '@Button[desc^="翻译成中文" && visibleToUser=true] <<n [vid="results"]',
           exampleUrls: 'https://e.gkd.li/c16f1e91-7c74-46d4-bcb2-dc15e9b6812d',
           snapshotUrls: 'https://i.gkd.li/i/18287650',
         },
